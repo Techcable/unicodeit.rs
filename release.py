6fc3b14bfd081475640c5d6a0146989ca1ac9275
Techcable <git@techcable.net>
2025-03-14 18:07:56 -0700

    Run formatting on release.py
    Missed doing this the first time.
    I normally have lefthook check formatting on commit,
    but I guess I forgot to set it up.

--- release.py
+++ release.py
@@ -4,12 +4,14 @@
 Uses `zigbuild` for cross compilation."""
 
 import argparse
-import subprocess
 import shutil
+import subprocess
 from pathlib import Path
+
 
 def binary_name(*, target: str, version: str) -> str:
     return f"unicodeit-{version}-{target}"
+
 
 SUPPORTED_PLATFORMS = [
     # TODO: No windows support
@@ -18,19 +20,30 @@ SUPPORTED_PLATFORMS = [
     "x86_64-unknown-linux-musl",
     "aarch64-unknown-linux-musl",
 ]
+
+
 def main():
     parser = argparse.ArgumentParser(
         description=__doc__,
     )
-    parser.add_argument("--out", help="Where to place the built files", default="staging")
-    parser.add_argument("--version", help="The version that is being built", required=True)
-    parser.add_argument("--no-clean", help="Do not run `cargo clean`", dest='clean', action='store_false')
+    parser.add_argument(
+        "--out", help="Where to place the built files", default="staging"
+    )
+    parser.add_argument(
+        "--version", help="The version that is being built", required=True
+    )
+    parser.add_argument(
+        "--no-clean",
+        help="Do not run `cargo clean`",
+        dest="clean",
+        action="store_false",
+    )
     args = parser.parse_args()
 
     out_dir = Path(args.out)
     if args.clean:
         print("Running `cargo clean`")
-        subprocess.run(['cargo', '+stable', 'clean'], check=True)
+        subprocess.run(["cargo", "+stable", "clean"], check=True)
 
     if out_dir.is_dir():
         shutil.rmtree(out_dir)
@@ -39,7 +52,15 @@ def main():
     for target in SUPPORTED_PLATFORMS:
         print(f"Building for {target!r}")
         subprocess.run(
-            ['cargo', '+stable', 'zigbuild', '--target', target, '--workspace', '--release'],
+            [
+                "cargo",
+                "+stable",
+                "zigbuild",
+                "--target",
+                target,
+                "--workspace",
+                "--release",
+            ],
             check=True,
         )
         target_build_dir = Path("target", target, "release")
@@ -48,17 +69,18 @@ def main():
         assert desired_binary.is_file()
         result_binary_path = out_dir / binary_name(target=target, version=args.version)
         shutil.copy2(desired_binary, result_binary_path)
-        with open(f'{result_binary_path}.sha256sum', 'wt') as hashfile:
+        with open(f"{result_binary_path}.sha256sum", "wt") as hashfile:
             subprocess.run(
-                ['sha256sum', result_binary_path],
+                ["sha256sum", result_binary_path],
                 check=True,
                 stdout=hashfile,
             )
         subprocess.run(
-            ['gpg', "--armor", "--detach-sign", result_binary_path.name],
+            ["gpg", "--armor", "--detach-sign", result_binary_path.name],
             cwd=out_dir,
             check=True,
         )
+
 
 if __name__ == "__main__":
     main()
